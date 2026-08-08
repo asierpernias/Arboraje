f1b57dd6740787f215980d7d26e8fcde3e610f5f
APSwinner <ague_soria@yahoo.es>
2026-08-08 20:13:52 +0200

    Random/Multiple points points

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ import math
 from PIL import ImageDraw, Image
 import random
 
-def interpretar(angulo, longitud, cadena, height, width ):
+def randompoints(value, width, height):
+    i = 0
+    cords = []
+    while i < value:
+        startx = random.randint(20, width)
+        starty = random.randint(20, height)
+        cords.append([startx, starty])
+        i += 1
+    return cords
+
+def interpretar(startx, starty, angulo, longitud, cadena, height, width ):
     anguloactual = -90
-    x = width/2
-    y = height/2
+    x = startx
+    y = starty
     profundidad = 4
     stack = []
     lines = []
+    fuera = False
     for char in cadena:
         if char == "F":
             prex = x
             prey = y
             x = x + longitud * math.cos(math.radians(anguloactual))
             y = y + longitud * math.sin(math.radians(anguloactual))
-            lines.append(((prex, prey), (x, y), profundidad))
+            if x < 20 or x > width - 20 or y < 20 or y > height - 20:
+                fuera = True
+                x, y = prex, prey  # revertir
+            if not fuera:
+                lines.append(((prex, prey), (x, y), profundidad))
         elif char == "+":
             anguloactual += (random.gauss(0, 15) + angulo)
         elif char == "-":
@@ -24,6 +39,7 @@ def interpretar(angulo, longitud, cadena, height, width ):
             profundidad -= 1
             stack.append((x, y, anguloactual))
         elif char == "]":
+            fuera = False
             profundidad += 1
             x, y, anguloactual = stack.pop()
     return lines
@@ -52,19 +68,25 @@ def expand(iteraciones, cadena, rules):
     
 
 def main():
-    angulo = int(input("Angle:"))
+    value = int(input("Number of points:"))
+    angulo = random.randint(0, 360)
     longitud = int(input("Length:"))
     cadena = input("Cadena:")
     width = int(input("Width:"))
     height = int(input("Height:"))
     iteraciones = int(input("Iteraciones:"))
     rules = {"F": "F[+F]F[-F]F"}
+    points = randompoints(value, width, height)
     cadena = expand(iteraciones, cadena, rules)
     print(cadena[:100])
-    lines = interpretar(angulo, longitud, cadena, height, width)
+    alllines = []
+    for punto in points:
+        startx, starty = punto
+        lines = interpretar(startx, starty, angulo, longitud, cadena, height, width)
+        alllines.extend(lines)
     print(len(lines))
     print(lines[:3])
-    renderlines(lines, width, height)
+    renderlines(alllines, width, height)
 
 if __name__ == "__main__":
     main()
